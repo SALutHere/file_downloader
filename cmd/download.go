9fd4f92a55503374b0862ac55fb6a9e8b5cb1b58
SALutHere <leetmansup@gmail.com>
2025-11-18 00:41:52 +0300

    Update download cmd
    Created download command skeleton

--- cmd/download.go
+++ cmd/download.go
@@ -1,36 +1,70 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
+var (
+	outputPath string
+	threads    int
+	resume     bool
+)
+
 // downloadCmd represents the download command
 var downloadCmd = &cobra.Command{
-	Use:   "download",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:   "download <url>",
+	Short: "Download file by specified URL",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("url is not specified")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		url := args[0]
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("download called")
+		fmt.Println("URL:", url)
+		fmt.Println("Output:", outputPath)
+		fmt.Println("Threads:", threads)
+		fmt.Println("Resume:", resume)
+
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(downloadCmd)
 
-	// Here you will define your flags and configuration settings.
+	downloadCmd.Flags().StringVarP(
+		&outputPath,
+		"output",
+		"o",
+		"",
+		"Path to save file",
+	)
+	downloadCmd.Flags().IntVarP(
+		&threads,
+		"threads",
+		"t",
+		4,
+		"Number of threads of downloading",
+	)
+	downloadCmd.Flags().BoolVar(
+		&resume,
+		"resume",
+		false,
+		"Allow file download to continue if there is a status",
+	)
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// downloadCmd.PersistentFlags().String("foo", "", "A help for foo")
+	downloadCmd.MarkFlagsMutuallyExclusive("resume")
+	downloadCmd.Flags().SortFlags = true
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// downloadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	downloadCmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		fmt.Println("Usage: file_downloader download <url> [flags]")
+		fmt.Println("\nFlags:")
+		cmd.Flags().PrintDefaults()
+	})
 }
